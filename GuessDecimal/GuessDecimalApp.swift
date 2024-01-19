2b266da1f08404a820b95b0c18b713e69acae0c8
6410685041 <6410685041@student.tu.ac.th>
2024-01-19 14:10:03 +0700

    Initial Commit

---
+++ GuessDecimal/GuessDecimalApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessDecimalApp.swift
+//  GuessDecimal
+//
+//  Created by นางสาวชลิศา ธรรมราช on 19/1/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct GuessDecimalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
